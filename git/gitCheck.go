507ea904e09c29db4de82b2201a82d02f98491d3
s.noirot <s.noirot@sportradar.com>
2023-10-29 11:29:30 +0100

    Refactor git commands

--- git/gitCheck.go
+++ git/gitCheck.go
@@ -1,27 +1,24 @@
 package git
 
 import (
+	"dx-cli/utils"
 	"fmt"
+	"github.com/spf13/cobra"
 	"os/exec"
 	"runtime"
-	"strings"
-
-	"github.com/spf13/cobra"
 )
 
 var checkGitCmd = &cobra.Command{
 	Use:   "check",
 	Short: "Check for Git installation",
+	Long:  "This command checks for the presence of Git on your system and offers to install it if it's not found.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		_, err := exec.LookPath("git")
 		if err != nil {
-			fmt.Println("Git is not installed. Would you like to install it? (y/n)")
-			var response string
-			fmt.Scanln(&response)
-			if strings.ToLower(response) == "y" {
+			if utils.PromptUser("Git is not installed. Would you like to install it?? 🎵", []string{"Yes", "No"}) == "Yes" {
 				return installGit()
 			}
-			return fmt.Errorf("Git is required for this program to function")
+			return fmt.Errorf("Git is required for this program to function.")
 		}
 
 		versionOutput, err := exec.Command("git", "--version").Output()
@@ -29,24 +26,24 @@ var checkGitCmd = &cobra.Command{
 			return err
 		}
 
-		fmt.Printf("Git is installed: %s\n", string(versionOutput))
+		utils.LogInfo(fmt.Sprintf("Git is installed: %s", string(versionOutput)))
 		return nil
 	},
 }
 
 func installGit() error {
+	var err error
 	switch runtime.GOOS {
 	case "darwin":
-		fmt.Println("Installing Git via Homebrew...")
-		err := exec.Command("brew", "install", "git").Run()
-		return err
+		utils.LogInfo("Installing Git via Homebrew...")
+		err = exec.Command("brew", "install", "git").Run()
 	case "linux":
-		fmt.Println("Installing Git via apt-get...")
-		err := exec.Command("sudo", "apt-get", "install", "-y", "git").Run()
-		return err
+		utils.LogInfo("Installing Git via apt-get...")
+		err = exec.Command("sudo", "apt-get", "install", "-y", "git").Run()
 	default:
-		return fmt.Errorf("Unsupported OS. Please install Git manually.")
+		err = fmt.Errorf("Unsupported OS. Please install Git manually.")
 	}
+	return err
 }
 
 func init() {
--- git/gitCmd.go
+++ git/gitCmd.go
@@ -6,5 +6,6 @@ import (
 
 var GitCmd = &cobra.Command{
 	Use:   "git",
-	Short: "Manage git",
+	Short: "Manage Git operations",
+	Long:  "The 'git' command provides a set of sub-commands to manage various Git operations such as testing SSH connections, registering SSH keys, and more.",
 }
